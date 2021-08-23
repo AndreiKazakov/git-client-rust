7c2687d165ff7fb420088c85d28d6f2abe0283ee
Andrei Kazakov <ay.kazakov89@gmail.com>
2021-08-23 20:36:31 +0200

    Implement trees

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,16 @@ fn main() -> Result<(), GitError> {
             let hash = write_object(Object::Blob(bytes))?;
             println!("{}", hash)
         }
+        "ls-tree" if args[2] == "--name-only" => match read_object(&args[3])? {
+            Object::Tree(refs) => print!(
+                "{}",
+                refs.iter()
+                    .map(|r| &*r.name)
+                    .collect::<Vec<&str>>()
+                    .join("\n")
+            ),
+            _ => return Err(GitError("Not a tree".to_owned())),
+        },
         _ => println!("unknown command: {}", args[1]),
     }
     Ok(())
@@ -30,12 +40,31 @@ fn main() -> Result<(), GitError> {
 
 enum Object {
     Blob(Vec<u8>),
+    Tree(Vec<ObjectReference>),
 }
 
 impl Object {
     fn content(&self) -> Result<String, GitError> {
         match self {
             Self::Blob(bytes) => Ok(std::str::from_utf8(bytes)?.to_owned()),
+            Self::Tree(refs) => {
+                let mut res = String::new();
+                for r in refs {
+                    res.push_str(&format!(
+                        "{} {} {}    {}",
+                        r.mode,
+                        if r.mode.to_string().starts_with('1') {
+                            "blob"
+                        } else {
+                            "tree"
+                        },
+                        to_hex(&r.hash)?,
+                        r.name
+                    ));
+                    res.push('\n')
+                }
+                Ok(res)
+            }
         }
     }
 
@@ -47,6 +76,21 @@ impl Object {
                 res.extend_from_slice(bytes.len().to_string().as_bytes());
                 res.push(b'\0');
                 res.extend(bytes);
+                res
+            }
+            Self::Tree(refs) => {
+                let mut res = Vec::new();
+                res.extend_from_slice(b"tree ");
+                let mut content = Vec::new();
+                for r in refs {
+                    content.extend_from_slice(r.mode.to_string().as_bytes());
+                    content.push(b' ');
+                    content.extend_from_slice(r.name.as_bytes());
+                    content.push(b'\0');
+                    content.extend(&r.hash);
+                }
+                res.extend_from_slice(content.len().to_string().as_bytes());
+                res.extend(content);
                 res
             }
         }
@@ -61,6 +105,34 @@ impl Object {
                     .skip(1)
                     .collect(),
             )),
+            b"tree" => {
+                let mut refs = Vec::new();
+                let mut i = bytes
+                    .iter()
+                    .position(|&b| b == b'\0')
+                    .ok_or("No null character found in tree object")?
+                    + 1;
+
+                while i < bytes.len() {
+                    let mode_bytes = bytes[i..]
+                        .iter()
+                        .take_while(|&&b| b != b' ')
+                        .copied()
+                        .collect::<Vec<u8>>();
+                    let mode: usize = std::str::from_utf8(&mode_bytes)?.parse()?;
+                    i += mode_bytes.len() + 1;
+                    let null_pos =
+                        bytes[i..].iter().position(|&b| b == b'\0').ok_or_else(|| {
+                            GitError("No null character found in tree object".to_string())
+                        })?;
+                    let name = std::str::from_utf8(&bytes[i..i + null_pos])?.to_string();
+                    i += null_pos + 1;
+                    let hash: Vec<u8> = bytes[i..i + 20].to_vec();
+                    i += 20;
+                    refs.push(ObjectReference { mode, name, hash })
+                }
+                Ok(Self::Tree(refs))
+            }
             _ => Err(GitError(format!(
                 "Unsupported object type: {}",
                 std::str::from_utf8(
@@ -75,7 +147,7 @@ impl Object {
 }
 
 struct ObjectReference {
-    mode: u16,
+    mode: usize,
     name: String,
     hash: Vec<u8>,
 }
@@ -89,12 +161,16 @@ fn read_object(sha: &str) -> Result<Object, GitError> {
     Object::decode(content)
 }
 
-fn get_hex(string: Vec<u8>) -> Result<String, GitError> {
-    use std::fmt::Write;
-
+fn get_sha(string: &[u8]) -> Result<String, GitError> {
     let mut sha_one = Sha1::new();
     sha_one.update(string);
     let bytes = sha_one.finalize();
+    to_hex(&bytes)
+}
+
+fn to_hex(bytes: &[u8]) -> Result<String, GitError> {
+    use std::fmt::Write;
+
     let mut hash = String::with_capacity(bytes.len() * 2);
     for byte in bytes {
         write!(hash, "{:02x}", byte)?;
@@ -107,7 +183,7 @@ fn write_object(obj: Object) -> Result<String, GitError> {
     let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
     encoder.write_all(data.as_slice())?;
     let result = encoder.finish()?;
-    let hash = get_hex(data)?;
+    let hash = get_sha(&data)?;
 
     let dir = format!("./.git/objects/{}", &hash[0..2]);
     if fs::metadata(&dir).is_err() {
